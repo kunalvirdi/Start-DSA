83571474fd613d735d9591c48e23504f87c1cb23
Kunal Saini <kunalsaini22112@gmail.com>
2022-10-14 21:08:06 +0530

    second change

--- submissions/mrkunal7/Task 1/1.cpp
+++ submissions/mrkunal7/Task 1/1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 vector<int> getConcatenate(vector<int> &nums){
     vector<int> ans;
     for (int i = 0; i < nums.size();i++){
-        ans.push_back(nums[i]);  //For storing first n elements in ans vector.
+        ans.push_back(nums[i]);  //For storing first 'n' elements in 'ans' vector.
     }
     for (int i = 0; i < nums.size(); i++)
     {
